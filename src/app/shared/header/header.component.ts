740ed4ecb03ad87238befea4e565326b1cc1b3c4
andreyherrera <andreyherrerac@icloud.com>
2024-04-26 13:37:46 -0500

    Add footer component and scroll functionality to header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  @ViewChild("home") home!: ElementRef;
+  @ViewChild("about") about!: ElementRef;
+  @ViewChild("contact") contact!: ElementRef;
+  @ViewChild("portfolio") portfolio!: ElementRef;
+
+  scrollToComponent(component: ElementRef) {
+    component.nativeElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 }
