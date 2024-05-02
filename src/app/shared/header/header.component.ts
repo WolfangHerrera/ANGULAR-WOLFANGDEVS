2ac779610787d34142ceb4d54e5dbd3c4050e630
wolfangherrera <andreyherrerac@icloud.com>
2024-05-02 13:16:05 -0500

    Update experience section and remove scroll functionality from header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
@@ -9,16 +8,4 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
-  @ViewChild("home") home!: ElementRef;
-  @ViewChild("about") about!: ElementRef;
-  @ViewChild("contact") contact!: ElementRef;
-  @ViewChild("portfolio") portfolio!: ElementRef;
-
-  scrollToComponent(component: ElementRef) {
-    component.nativeElement.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  }
 }
