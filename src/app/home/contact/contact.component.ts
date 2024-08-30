79f5d3a98693c7665f2182d3bdfccffdb975f042
wolfangherrera <andreyherrerac@icloud.com>
2024-08-29 22:19:18 -0500

    "Updated package-lock.json, app.module.ts, contact.component.html, and contact.component.ts with various changes including renaming, imports, and form handling."

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ContactForm } from "./contact.model";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent {
+  contact: ContactForm = {
+    name: "",
+    subject: "",
+    message: "",
+  };
 
+  onSubmit(event: Event) {
+    event.preventDefault();  // Prevenir el comportamiento por defecto del formulario
+
+    const target = event.target as HTMLFormElement;
+    const activeElement = document.activeElement as HTMLButtonElement;
+
+    const buttonId = activeElement.id;  // Obtener el id del botón presionado
+
+    console.log('Formulario enviado', this.contact);
+    console.log('Botón presionado:', buttonId);
+
+    // Aquí puedes manejar la lógica según el botón presionado
+    if (buttonId === 'whatsapp-mobile' || buttonId === 'whatsapp-desktop') {
+      // Lógica para el botón de WhatsApp
+      console.log('Enviar mensaje de WhatsApp');
+    } else if (buttonId === 'email-mobile' || buttonId === 'email-desktop') {
+      // Lógica para el botón de Email
+      console.log('Enviar mensaje por Email');
+    }
+  }
 }
