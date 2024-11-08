879c1214751bf9764ae143840de2e43c06b3d908
wolfangherrera <andreyherrerac@icloud.com>
2024-11-08 18:17:32 -0500

    Add new components and styles for About, Experience, and Portfolio sections

---
+++ src/app/core/wolfangdevs/wolfangdevs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wolfangdevs',
+  templateUrl: './wolfangdevs.component.html',
+  styleUrls: ['./wolfangdevs.component.scss']
+})
+export class WolfangdevsComponent {
+
+}
