65cd3e2dba51740621957ca8d6a7be29340dcc72
wolfangherrera <andreyherrerac@icloud.com>
2024-11-09 10:35:01 -0500

    Enable strict versioning for Angular shared dependencies in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -31,31 +31,23 @@ module.exports = {
       shared: share({
         "@angular/core": {
           singleton: true,
-          strictVersion: false,
+          strictVersion: true,
           requiredVersion: "auto",
         },
         "@angular/common": {
           singleton: true,
-          strictVersion: false,
+          strictVersion: true,
           requiredVersion: "auto",
         },
         "@angular/common/http": {
           singleton: true,
-          strictVersion: false,
+          strictVersion: true,
           requiredVersion: "auto",
         },
         "@angular/router": {
           singleton: true,
-          strictVersion: false,
+          strictVersion: true,
           requiredVersion: "auto",
-        },
-        "date-fns": {
-          singleton: true,
-          requiredVersion: "3.6.0",
-        },
-        sweetalert2: {
-          singleton: true,
-          requiredVersion: "11.10.7",
         },
         ...sharedMappings.getDescriptors(),
       }),
