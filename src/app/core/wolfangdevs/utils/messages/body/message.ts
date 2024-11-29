2bcca32bcdd9774431e0c79feb0d4a33e00bd9d6
wolfangherrera <andreyherrerac@icloud.com>
2024-11-28 20:32:56 -0500

    Update PDF component: adjust button icon sizes and standardize 'about' message

--- src/app/core/wolfangdevs/utils/messages/body/message.ts
+++ src/app/core/wolfangdevs/utils/messages/body/message.ts
@@ -1,17 +1,18 @@
 import { bodyMessages } from "../../type/messages/body/message.type";
 
 const name = "WOLFANG HERRERA";
+const about = "SOFTWARE DEVELOPER";
 
 export const ES_BODY_MESSAGES: bodyMessages = {
   greeting: "HOLA, ¡SOY YO!",
   name: name,
-  about: "DESARROLLADOR FULL-STACK",
+  about: about,
   button: "DESCARGAR CV",
 };
 
 export const EN_BODY_MESSAGES: bodyMessages = {
   greeting: "HEY, IT'S ME!",
   name: name,
-  about: "AND I'M FULL-STACK DEVELOPER",
+  about: about,
   button: "DOWNLOAD CV",
 };
