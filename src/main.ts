17298a047fd1383b15f29883365f20aaec413f7e
wolfangherrera <andreyherrerac@icloud.com>
2024-12-29 18:31:35 -0500

    Add service worker registration in main.ts and remove unnecessary whitespace in index.html

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('ngsw-worker.js')
+      .then(registration => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch(error => {
+        console.error('Error al registrar el Service Worker:', error);
+      });
+  });
+}
+
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
